48c8d31f6038f7c5f45c28cfc3a9b7509270aae3
Paul Beaudet <inof8or@gmail.com>
2016-11-05 19:55:06 -0400

    make whitespace consistent, remove unneeded code, provide some library links

--- botsquare/botsquare.ino
+++ botsquare/botsquare.ino
@@ -1,35 +1,28 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *RightMotor2 = AFMS.getMotor(2);
 // You can also make another motor on port M2
 Adafruit_DCMotor *LeftMotor1 = AFMS.getMotor(1);
 
 void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
-
-  AFMS.begin();  // create with the default frequency 1.6KHz 
+  Serial.begin(9600);  // set up Serial library at 9600 bps
+  Serial.println("go in a square test!");
+  AFMS.begin();        // create with the default frequency 1.6KHz
 }
 
 void loop() {
-  uint8_t i;
- 
   LeftMotor1->setSpeed(110);
   RightMotor2->setSpeed(110);
-  
+
   RightMotor2->run(BACKWARD);
   LeftMotor1->run(BACKWARD);
-
   delay(1800);
-  
+
   LeftMotor1->setSpeed(5);
   RightMotor2->setSpeed(125);
- 
   delay(525);
 }
--- botwithtwosensors/botwithtwosensors.ino
+++ botwithtwosensors/botwithtwosensors.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_PWMServoDriver.h"
-#include <NewPing.h>
+#include <NewPing.h> // http://playground.arduino.cc/Code/NewPing
+
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
@@ -9,116 +9,83 @@ Adafruit_DCMotor *RightMotor2 = AFMS.getMotor(2);
 // You can also make another motor on port M2
 Adafruit_DCMotor *LeftMotor1 = AFMS.getMotor(1);
 
-#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define TRIGGER_PINrt  8  // 8 Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PINrt     10  // 10 Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 140 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define TRIGGER_PIN   12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN      11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PINrt 8   // 8 Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PINrt    10  // 10 Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE  140 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define LED 13
 #define inPin 2
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);      // NewPing setup of pins and maximum distance.
 NewPing sonar2(TRIGGER_PINrt, ECHO_PINrt, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-
 boolean boolRight = false;
 
 void setup() {
-AFMS.begin(); // create with the default frequency 1.6KHz
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
- 
+  AFMS.begin();       // create with the default frequency 1.6KHz
+  Serial.begin(9600); // Open serial monitor to see ping results.
 }
 
 void loop() {
-uint8_t i;
+  delay(75); // Wait X ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uSRight = sonar2.ping(); // Send ping, get ping time in microseconds (uS)
+  unsigned int uSLeft = sonar.ping(); // Send ping, get ping time in microseconds (uS)
+  // Convert ping time to distance in cm and print result (0 = outside set distance range)
+  Serial.print(F("PingRight: ")); Serial.println(uSRight ); Serial.print("cm");
+  Serial.print(F("PingLeft: ")); Serial.print(uSLeft ); Serial.println("cm");
 
-delay(75); 
-//delay(200); 
-                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-     unsigned int uSright = sonar2.ping(); // Send ping, get ping time in microseconds (uS).
-//  Serial.print("PingRight: ");
- //   Serial.println(uSright ); // C  
-//  
-//delay(75); 
+  int tglOn = digitalRead(inPin);
 
-  //right sensor
-                                                            //convert ping time to distance in cm and print result (0 = outside set distance range)
- unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
-  Serial.print("Ping: ");
-   Serial.print(uS ); // Convert ping time to distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
- //delay(75);  
-
-int tglOn = digitalRead(inPin);
-
-if ( uS < 850 && uS > 200) {
-//  
-//
+  if ( uSLeft < 850 && uSLeft > 200) {
     rotateLeft();
     digitalWrite(LED,HIGH);
     delay(700);
     digitalWrite(LED,LOW);
-    
-//    boolRight = false;
-//  }
- } else if ( uSright < 850 && uSright > 200 )  {
-  //if ( uSright < 850 && uSright > 200 )  { 
-   rotateRight();
+  } else if ( uSRight < 850 && uSRight > 200 )  {
+    rotateRight();
     digitalWrite(LED,HIGH);
     delay(700);
     digitalWrite(LED,LOW);
-   
-//} else if ( (uSright < 850 && uSright > 200) && (uS < 850 && uS > 200))  {
- } else if ( tglOn == HIGH )  {
- 
-   goBackward();
-   digitalWrite(LED,HIGH);
+  } else if ( tglOn == HIGH )  {
+    goBackward();
+    digitalWrite(LED,HIGH);
     delay(700);
     digitalWrite(LED,LOW);
- 
- } else {
-   
-  goForward();
-   
- }
-  
-  
-  
-  
+  } else {
+    goForward();
+  }
 }
 
-void goForward ()
-{
-LeftMotor1->setSpeed(120);
-RightMotor2->setSpeed(120);
-RightMotor2->run(BACKWARD);
-LeftMotor1->run(BACKWARD);
+void goForward (){
+  LeftMotor1->setSpeed(120);
+  RightMotor2->setSpeed(120);
+  RightMotor2->run(BACKWARD);
+  LeftMotor1->run(BACKWARD);
 }
  
-void rotateLeft ()
-{
-LeftMotor1->setSpeed(50);
-RightMotor2->setSpeed(120);
-RightMotor2->run(FORWARD);
-LeftMotor1->run(FORWARD);
-//RightMotor2->run(RELEASE);
-//LeftMotor1->run(RELEASE);
+void rotateLeft(){
+  LeftMotor1->setSpeed(50);
+  RightMotor2->setSpeed(120);
+  RightMotor2->run(FORWARD);
+  LeftMotor1->run(FORWARD);
 }
 
-void rotateRight ()
-{
-LeftMotor1->setSpeed(120);
-RightMotor2->setSpeed(50);
-RightMotor2->run(FORWARD);
-LeftMotor1->run(FORWARD);
-//RightMotor2->run(RELEASE);
-//LeftMotor1->run(RELEASE);
+void rotateRight(){
+  LeftMotor1->setSpeed(120);
+  RightMotor2->setSpeed(50);
+  RightMotor2->run(FORWARD);
+  LeftMotor1->run(FORWARD);
 }
 
-void goBackward ()
-{
-LeftMotor1->setSpeed(130);
-RightMotor2->setSpeed(130);
-RightMotor2->run(FORWARD);
-LeftMotor1->run(BACKWARD);
+void goBackward(){
+  LeftMotor1->setSpeed(130);
+  RightMotor2->setSpeed(130);
+  RightMotor2->run(FORWARD);
+  LeftMotor1->run(BACKWARD);
 }
+
+void stopMotors(){
+  RightMotor2->run(RELEASE);
+  LeftMotor1->run(RELEASE);
+}
+
